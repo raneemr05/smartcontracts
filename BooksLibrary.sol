3f8a661bcefcd0e8efce28a635e02ae06cfa66df
raneemr05 <57775695+raneemr05@users.noreply.github.com>
2022-09-20 22:53:06 +0100

    Create BooksLibrary.sol

---
+++ BooksLibrary.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.8.11;
+
+contract bookLibrary {
+    address public contractOwner;
+
+    constructor() {
+        contractOwner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == contractOwner);
+        _;
+    }
+
+    struct Book {
+        uint256 id;
+        string name;
+        string author;
+        uint256 price;
+    }
+
+    mapping(uint256 => Book) public books;
+
+    function addBook(
+        uint256 _bookId,
+        string memory _bookName,
+        string memory _bookAuthor,
+        uint256 _price
+    ) public onlyOwner {
+        books[_bookId].name = _bookName;
+        books[_bookId].author = _bookAuthor;
+        books[_bookId].price = _price;
+    }
+
+    function queryBookById(uint256 _bookId)
+        public view returns (
+            string memory name,
+            string memory author,
+            uint256 price
+        )
+    {
+        return (
+            books[_bookId].name,
+            books[_bookId].author,
+            books[_bookId].price
+        );
+    }
+}
